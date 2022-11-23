77c217484b7461f58b3c50adbc9451d40ab84ca5
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-23 18:29:30 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMatriz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Digite a quantidade de números da matriz, em seguida digite os valores da matriz para o programa somar os elementos da linha:  ");
+
+
+            int M, N;
+            int[,] mat;
+
+            string[] s = Console.ReadLine().Split(' ');
+            M = int.Parse(s[0]);
+            N = int.Parse(s[1]);
+            mat = new int[M, N];
+
+            for(int i= 0; i < M; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for(int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            int[] vet = new int[M];
+
+            for (int i= 0; i < M; i++)
+            {
+                int soma = 0;
+                for (int j= 0; j < N; j++)
+                {
+                    soma = soma + mat[i, j];
+                }
+                vet[i] = soma;
+            }
+
+            for (int i=0; i < M; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
